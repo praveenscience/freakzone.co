04c5e16d267bde169723b97c444846dfde13e339
Praveen Kumar <praveenscience@gmail.com>
2021-10-31 13:45:09 +0000

    Defined a couple of hot points for the images.
    * Last image is the winning image.
    * No need to define them for the last one.

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,12 @@ const App = () => {
     "https://i.imgur.com/c3B6qHK.png",
     "https://i.imgur.com/6Ssj5XX.png"
   ];
+  const HotPoints = [
+    [125, 50, 205, 125],
+    [330, 45, 410, 115],
+    [835, 535, 860, 560],
+    [0, 0, 0, 0]
+  ];
   const check = (x, y, arr) => {
     const [x1, y1, x2, y2] = arr;
     return x <= x2 && x >= x1 && y <= y2 && y >= y1;
