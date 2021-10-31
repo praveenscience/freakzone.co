771971830930ca1e79a994de5d14441958e63585
Praveen Kumar <praveenscience@gmail.com>
2021-10-31 13:41:13 +0000

    Added a random spooky image.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,9 @@ const App = () => {
   return (
     <div className="App">
       <h1>Find Stuff!</h1>
+      <div className="Image">
+        <img src="https://i.imgur.com/PahL6NP.png" alt="Find Stuff!" />
+      </div>
     </div>
   );
 };
