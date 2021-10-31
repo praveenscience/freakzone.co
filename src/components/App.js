83ff88b83b14fd3d48ce08ef07a1abfff3b4996f
Praveen Kumar <praveenscience@gmail.com>
2021-10-31 13:42:00 +0000

    Created a set of images.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 const App = () => {
+  const Images = [
+    "https://i.imgur.com/PahL6NP.png",
+    "https://i.imgur.com/hOmFOle.png",
+    "https://i.imgur.com/c3B6qHK.png",
+    "https://i.imgur.com/6Ssj5XX.png"
+  ];
   return (
     <div className="App">
       <h1>Find Stuff!</h1>
